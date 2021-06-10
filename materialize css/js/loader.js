9fde2a54a63229f03e5ffad2fb97959bbd886e43
Rupam Shil <rupamshil111@gmail.com>
2021-06-10 16:47:43 +0530

    materialize css done

--- materialize css/js/loader.js
+++ materialize css/js/loader.js
@@ -1,6 +1,10 @@
 $(document).ready(function () {
 	$('.modal').modal();
 	$('.parallax').parallax();
+	$('.sidenav').sidenav();
+	$('.slider').slider({ full_width: true });
+	$('.parallax').parallax();
+	$('.myreviews').carousel({ numVisible: 5, shift: 55, padding: 55 });
 });
 
 function toggleModal() {
